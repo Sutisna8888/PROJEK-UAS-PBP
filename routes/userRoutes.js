d4d59479b17ee1c0a7c6407e9683a253db49e782
Sutisna8888 <Sutisna9215@gmail.com>
2025-01-18 18:37:08 +0700

    menambahkan endpoint comments dan likes

---
+++ routes/userRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const {
+  getUser,
+  getProfile,
+  userRegister,
+  userLogin,
+  buatPost,
+  userComments,
+  getcomment,
+  deletComment,
+  userLike,
+  deletlike,
+} = require("../controllers/userControllers");
+
+const router = express.Router();
+
+router.get("/", getUser);
+router.get("/profile/:id", getProfile);
+router.post("/register", userRegister);
+router.post("/login", userLogin);
+router.post("/post", buatPost);
+router.post("/comment", userComments);
+router.get("/comment/:PostID", getcomment);
+router.delete("/comment/:CommentID", deletComment);
+router.post("/like", userLike);
+router.delete("/like/:LikeID", deletlike)
+
+
+module.exports = router;
